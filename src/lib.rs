2582e7c9d85d9d3bd7fb6f051ae4eaa23ff3d2a2
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2022-07-07 23:19:20 +0200

    Implement interact for Read/Write

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,26 @@
+use std::io::{Read, Write};
+
+pub fn interact(
+    mut r: impl Read,
+    mut w: impl Write,
+    f: impl Fn(String) -> String,
+) -> Result<(), std::io::Error> {
+    let mut input = String::new();
+    r.read_to_string(&mut input)?;
+    let output = f(input);
+    w.write_all(output.as_bytes())
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn interact_with_write_read_traits() {
+        let mut input = b"1234".as_ref();
+        let mut output = Vec::<u8>::new();
+        let res = interact(&mut input, &mut output, |x| x.chars().rev().collect());
+        assert!(res.is_ok());
+        assert_eq!(b"4321".as_ref(), &output);
     }
 }
